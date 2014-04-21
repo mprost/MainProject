3a484727c652069ed190289be9fdc1fdb6d79c58
mprost <mprost9@aol.com>
2014-04-21 16:58:17 -0500

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,11 @@ public class Test
 
 		public static void main(String[] args)
 			{
+				for(int x=0; x<2; x++)
+				{
+					x-=2;
+					//hehe
+				}
 				//PROST WAS HERE
 				System.out.println("F you all!");
 
